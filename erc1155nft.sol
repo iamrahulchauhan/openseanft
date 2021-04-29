73c23ee29b5151b4df47abb9c580f2bdab5328c8
Rahul Chauhan <iamrahulchauhan17@gmail.com>
2021-04-29 15:54:57 +0530

    Update erc1155nft.sol

--- erc1155nft.sol
+++ erc1155nft.sol
@@ -3,16 +3,15 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 
-contract erc1155nft is erc1155 {
+contract erc1155nft is ERC1155 {
     //TWO NFTs with erc1155 standard. 
-    uint256 public constant GOLD = 0;
-    uint256 public constant SILVER = 1;
+    uint256 public constant GOLD = 1;
+    uint256 public constant SILVER = 2;
     
-    
-   
+
     constructor(
        string memory uri
-    ) ERC1155("Contract Uri Here") public {
+    ) ERC1155("ipfs://QmP5UWFnW2sJsSL37djNuGWjVb5b7jih6yWc3HsqF41P8g/GOLD_Metadata.json") public {
         
         //minting the ERC1155 tokens.     
         _mint(msg.sender, GOLD,75 , "");   
@@ -21,8 +20,12 @@ contract erc1155nft is erc1155 {
 
       
     }
-function return_uri() public view returns (string) {
-    return "Contract Uri Here";
-}
     
+    function return_uri() public view returns (string memory) {
+    
+    return "ipfs://QmP5UWFnW2sJsSL37djNuGWjVb5b7jih6yWc3HsqF41P8g/GOLD_Metadata.json";
+    
+        
+    }
+
 }
